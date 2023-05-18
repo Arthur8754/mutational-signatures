54ea1d824842f0c50280b7fbb5a6c5a73682c7eb
Arthur Baudot <arthur.baudot@telecomnancy.eu>
2023-05-18 11:45:06 -0400

    First Cox implementation (not good)

--- survivalAnalysis/CoxRegression.py
+++ survivalAnalysis/CoxRegression.py
@@ -1,6 +1,17 @@
+from sksurv.linear_model import CoxPHSurvivalAnalysis
+
 class CoxRegression:
     """ 
     Code for implementing the Cox Regression Method which estimates the hazard function.
     """
     def __init__(self) -> None:
         pass
+
+    def compute_hazard_function(self, X, y):
+        """ 
+        Compute the coefficients of the Cox Regression model.
+        """
+        cph = CoxPHSurvivalAnalysis()
+        cph.fit(X, y)
+        hazard_function = cph.predict_cumulative_hazard_function(X)
+        return hazard_function

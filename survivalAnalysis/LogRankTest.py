3a2107569b7012ee3540bee271eb16cfe36b6122
Arthur Baudot <arthur.baudot@telecomnancy.eu>
2023-05-26 14:34:17 -0400

    add p value and first split dcb-ncb

--- survivalAnalysis/LogRankTest.py
+++ survivalAnalysis/LogRankTest.py
@@ -9,18 +9,18 @@ class LogRankTest:
     def __init__(self) -> None:
         pass
     
-    def stat_test(self, survival_group1: pd.Series, status_group1: pd.Series, survival_group2: pd.Series, status_group2: pd.Series) -> float:
+    def make_test(self, time_survival_1: np.ndarray, status_survival_1: np.ndarray, time_survival_2: np.ndarray, status_survival_2: np.ndarray) -> float:
         """ 
         Make the log rank test : tests if the group1 and group2 curves are similar.
 
         ### Parameters :
-        - survival_group1 : the time survival of group 1
-        - status_group1 : the status survival of group 1 (1 if event observed)
-        - survival_group2 : the time survival of group 2
-        - status_group2 : the status survival of group 2 (1 if event observed)
+        - time_survival_1 : the time survival of group 1
+        - status_survival_1 : the status survival of group 1 (1 if event observed)
+        - time_survival_2 : the time survival of group 2
+        - status_survival_2 : the status survival of group 2 (1 if event observed)
 
         ### Returns :
         The p value of the log rank test.
         """
-        test_results = logrank_test(survival_group1, survival_group2, status_group1, status_group2, alpha=0.95)
+        test_results = logrank_test(time_survival_1, time_survival_2, status_survival_1, status_survival_2, alpha=0.95)
         return np.round(test_results.p_value,3)

4b3d8e1573c721d0cf438bf471e7eb2d05054f9a
Arthur Baudot <arthur.baudot@telecomnancy.eu>
2023-06-15 08:15:12 -0400

    Graph construction

--- models/BuildGraph.py
+++ models/BuildGraph.py
@@ -1,74 +1,98 @@
 import numpy as np
 import pandas as pd
-from sklearn.neighbors import NearestNeighbors
 import networkx as nx
 import matplotlib.pyplot as plt
 
 class BuildGraph:
     """ 
-    This class creates a graph from an initial dataframe, with the k-NN algorithm. Each node is connected to its k nearest neighbors.
+    This class creates a graph from an initial dataframe. The graph is an union of distinct complete sub graphs, where each sub graph 
+    contains the patients with the same value of a specific column.
     """
-    def __init__(self, k: int, metric: str) -> None:
+    def __init__(self, df: pd.DataFrame) -> None:
+        """ 
+        - df : the source dataframe to build the graph 
         """
-        - k : the number of neighbors for each node in the graph.
-        - metric ("euclidean","cosine","manhattan") : the metric used to compute distance between points.
-        """
-        self.k = k+1
-        self.metric=metric
-        self.model = NearestNeighbors(n_neighbors=k+1, metric=metric)
+        self.df = df
 
-    def apply_knn(self, X: np.ndarray)->np.ndarray:
-        """
-        Apply the k-NN algorithm to connect each node to its k nearest neighbors.
+    def split_per_column(self, column_name: str, min_rows: int)->dict[str,pd.DataFrame]:
+        """ 
+        Split the dataframe df into distinct groups, depending on the column_name values. Merge if the sub dataframe contains less than min_rows.
 
         ### Parameters :
-        - X (n_samples, n_features) : the points to connect, with their features.
+        - column_name : the name of the column used to make the split.
+        - min_rows : the minimum number of rows required to 
 
         ### Returns :
-        The adjacency matrix of the graph.
+        A dictionary, where key = column value, and value = sub dataframe.
         """
-        # Need to have n_samples >= n_neighbors
-        if X.shape[0]<self.k:
-            return np.zeros((X.shape[0],X.shape[0]))
-        
-        # Find the k nearest neighbors of each patient
-        self.model.fit(X)
+        column_values = np.unique(self.df[column_name].to_numpy())
+        dico = {}
+        for i in range(column_values.shape[0]):
+            # Sub dataframe which contains the same specific values
+            sub_df = self.df.loc[self.df[column_name] == column_values[i]]
 
-        # Build the associated adjacency matrix
-        A = self.model.kneighbors_graph(X).toarray()
-        return A-np.identity(A.shape[0])
+            # Merge if not enough rows
+            if sub_df.shape[0]<min_rows:
+                if "merged" not in dico:
+                    dico["merged"] = sub_df 
+                else:
+                    dico["merged"] = pd.concat([dico["merged"],sub_df])
+
+            else:
+                dico[column_values[i]] = self.df.loc[self.df[column_name] == column_values[i]]
+        return dico
     
-    def merge_adjacency_matrix(self, matrices:list[np.ndarray])->np.ndarray:
+    def local_adjacency_matrix(self, sub_df: pd.DataFrame)->np.ndarray:
         """ 
-        Merge the local adjacency matrices for subgraphs into global one for whole graph.
+        Compute the local adjacency matrix of the sub_df fully connected graph.
 
+        ### Parameters :
+        - sub_df : the source sub dataframe to build the complete sub graph.
+
+        ### Returns :
+        - the adjacency matrix of this complete sub graph.
+        """
+        # The adjacency matrix of a complete graph is a matrix of 1.
+        shape_A = sub_df.shape[0]
+        return np.ones((shape_A,shape_A))-np.identity(shape_A)
+    
+    def global_adjacency_matrix(self, dico_df: dict[str, pd.DataFrame])->np.ndarray:
+        """ 
+        Build the global adjacency matrix of the graph with merging the local matrices.
         ### Parameters :
         - matrices : the local adjacency matrices of the distinct subgraphs.
 
         ### Returns ;
+        
         The global adjacency matrix for the whole graph.
         """
-        #shape_A = np.sum([matrices[i].shape[0] for i in range(len(matrices))])
-        A = matrices[0]
+        keys = list(dico_df.keys())
 
-        # Iteratively merge each matrix
-        for i in range(1,len(matrices)):
-            shape_A, shape_i = A.shape[0], matrices[i].shape[0]
+        # Initialize the global adjacency matrix
+        A_global = self.local_adjacency_matrix(dico_df[keys[0]])
 
-            # Concatenation of A and the next matrix along diagonal
-            A = np.block([
-                [A,np.zeros((shape_A, shape_i))],
-                [np.zeros((shape_i, shape_A)),matrices[i]]
+        # Iteratively merge local adjacency matrices
+        for i in range(1,len(keys)):
+            # Local adjacency matrix
+            A_local = self.local_adjacency_matrix(dico_df[keys[i]])
+
+            shape_A_global, shape_A_local = A_global.shape[0], A_local.shape[0]
+
+            # Concatenation of A_global and A_local along diagonal
+            A_global = np.block([
+                [A_global,np.zeros((shape_A_global, shape_A_local))],
+                [np.zeros((shape_A_local, shape_A_global)),A_local]
             ])
-
-        return A
+        return A_global
     
-    def show_graph(self, A: np.ndarray)->None:
+    def show_graph(self, A: np.ndarray, title: str, filename: str)->None:
         """ 
         Create the graph from the adjacency matrix and plot it.
 
         ### Parameters :
         - A (n_samples, n_samples) : the adjacency matrix of the graph
+        - title : the title of the figure
+        - filename : the location to store the figure
 
         ### Returns :
         None
@@ -76,33 +100,5 @@ class BuildGraph:
         G = nx.Graph(A)
         fig, ax = plt.subplots(figsize=(10,7))
         nx.draw(G)
-        plt.savefig('graph.png')
-
-    def proportion_similarity_neighbors(self, A:np.ndarray, feature:np.ndarray)->float:
-        """ 
-        Estimate the proportion of similar neighbor patients for the given feature.
-
-        ### Parameters :
-        - A (n_samples,n_samples) : The adjacency matrix of the graph.
-        - feature (n_samples,) : the feature value for each sample.
-
-        ### Returns :
-        The mean proportion of neighbor patients which are similar for the given feature.
-        """
-        similarity = 0
-        n=0
-        for i in range(A.shape[0]):
-            # Feature of patient i
-            feature_i = feature[i]
-
-            # Feature extraction of the neighbors of i
-            neighbors_i = feature[np.where(A[i] == 1)[0]]
-
-            # Count similar feature
-            if neighbors_i.shape[0]!=0:
-                n+=1
-                similarity += np.count_nonzero(np.where(neighbors_i == feature_i,True,False))/neighbors_i.shape[0]
-
-        if n!=0:
-            return np.round(similarity/n,2)
-        return 0
+        plt.title(title)
+        plt.savefig(filename)
